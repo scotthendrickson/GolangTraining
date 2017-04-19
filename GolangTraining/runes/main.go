00544de335b947e6ff19cf32241e0ca0ab8ebba2
Scott Hendrickson <shendrickson@localhost.localdomain>
2017-04-19 16:58:37 -0600

    More runes

--- GolangTraining/runes/main.go
+++ GolangTraining/runes/main.go
@@ -5,5 +5,6 @@ import "fmt"
 func main() {
 	for i := 5000; i <= 5100; i++ {
 		fmt.Println(i, " - ", string(i), " - ", []byte(string(i)))
+		fmt.Printf("%v - %v - %v \n", i, string(i), []byte(string(i)))
 	}
 }
