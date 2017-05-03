a2807ffcb8d89c0c52765413a15c3a570cd03aa7
Scott Hendrickson <shendrickson@localhost.localdomain>
2017-05-03 10:23:01 -0600

    Updating files

--- GolangTraining/09_remainder/main.go
+++ GolangTraining/09_remainder/main.go
@@ -10,10 +10,24 @@ func main() {
 
 	for i := 0; i < 100; i++ {
 		y := i % 2
+		fizzBuzz(i)
 		if y == 0 {
 			fmt.Println("Even")
 		} else {
 			fmt.Println("Odd")
 		}
+	}
+}
+
+func fizzBuzz(num int) {
+	switch {
+	case num%3 == 0:
+		fmt.Println("fizz")
+	case num%5 == 0:
+		fmt.Println("Buzz")
+	case num%15 == 0:
+		fmt.Println("FizzBuzz")
+	default:
+		fmt.Println(num)
 	}
 }
